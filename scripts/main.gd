f7bf5643fb2b82084734eff39ab79a016eb887b3
David Sadler <davidwallacesadler@gmail.com>
2024-08-30 15:18:27 -0600

    Add sum lighting

--- scripts/main.gd
+++ scripts/main.gd
@@ -10,6 +10,7 @@ extends Node2D
 # Private Variables
 # ////////////////////
 var _is_showing_player_pov: bool = false
+var _darkness_color: Color = Color(0.004, 0.145, 0.204)
 
 # ////////////////////
 # Virtual Functions
@@ -24,6 +25,7 @@ func _ready():
 	
 	%Player.position = player_start_position
 	%PlayerPov.current_tile_position = player_start_position_in_tiles
+	%WorldDarkness.color = _darkness_color
 	
 	GameEvents.player_interacted.connect(_on_player_interacted)
 	
@@ -45,11 +47,19 @@ func _check_and_handle_pov_button_pressed() -> void:
 func _check_and_handle_open_inventory_pressed() -> void:
 	if Input.is_action_just_pressed("open_inventory"):
 		%Inventory.visible = not %Inventory.visible
+		if %Inventory.visible:
+			%WorldDarkness.color = Color(1, 1, 1, 1)
+		elif not _is_showing_player_pov:
+			%WorldDarkness.color = _darkness_color
 
 func _update_scene_visiblities() -> void:
 	%World.visible = not _is_showing_player_pov
 	%PlayerPov.visible = _is_showing_player_pov
 	
+	if _is_showing_player_pov:
+		%WorldDarkness.color = Color(1, 1, 1, 1) 
+	else:
+		%WorldDarkness.color = _darkness_color
 
 func _on_player_interacted(data: InteractableData) -> void:
 	_is_showing_player_pov = true
